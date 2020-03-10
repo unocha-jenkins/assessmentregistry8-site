533c5b41a19791000124bb5c7daaf49e9e4e0f92
Peter Droogmans (attiks) <peter@attiks.com>
2020-03-10 17:07:24 +0100

    location ajax

--- html/modules/custom/ocha_integrations/ocha_locations/src/Plugin/Field/FieldWidget/OchaLocationsOptionsSelectWidget.php
+++ html/modules/custom/ocha_integrations/ocha_locations/src/Plugin/Field/FieldWidget/OchaLocationsOptionsSelectWidget.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\ocha_locations\Plugin\Field\FieldWidget;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\ReplaceCommand;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -22,12 +24,27 @@ class OchaLocationsOptionsSelectWidget extends WidgetBase {
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $form_state->setRebuild(true);
+dpm('build');
+dpm($form_state->get('level0'), 'state');
+    if ($form_state->get('level0')) {
+      $items[$delta]->level0 = $form_state->get('level0');
+    }
+
     $element['level0'] = array(
       '#type' => 'select',
       '#options' => ocha_locations_allowed_values_by_parent(),
       '#title' => t('Level 0'),
       '#default_value' => isset($items[$delta]->level0) ? $items[$delta]->level0 : '',
       '#empty_option' => t(' - Select admin level 0 - '),
+      '#ajax' => [
+        'callback' => [$this, 'changeLevel0'],
+        'event' => 'change',
+        'wrapper' => 'ocha-location-wrapper',
+        'progress' => array(
+          'type' => 'throbber',
+        ),
+      ]
     );
 
     if (isset($items[$delta]->level0)) {
@@ -58,14 +75,29 @@ class OchaLocationsOptionsSelectWidget extends WidgetBase {
 
     // If cardinality is 1, ensure a label is output for the field by wrapping
     // it in a details element.
-    if ($this->fieldDefinition->getFieldStorageDefinition()->getCardinality() != 1) {
+    if (FALSE && $this->fieldDefinition->getFieldStorageDefinition()->getCardinality() != 1) {
       $element += array(
         '#type' => 'fieldset',
         '#attributes' => array('class' => array('container-inline')),
       );
     }
 
+    $element['#prefix'] = '<div id="ocha-location-wrapper">';
+    $element['#suffix'] = '</div>';
+
     return $element;
+  }
+
+  public function changeLevel0(array &$form, FormStateInterface &$form_state){
+    dpm('changeLevel0');
+    $triggeringElement = $form_state->getTriggeringElement();
+    $form_state->set('level0', $triggeringElement['#value']);
+    $form_state->setRebuild();
+
+    $response = new AjaxResponse();
+    $response->addCommand(new ReplaceCommand('ocha-location-wrapper', $triggeringElement));
+
+    return $response;
   }
 
   public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
@@ -83,5 +115,4 @@ class OchaLocationsOptionsSelectWidget extends WidgetBase {
 
     return $values;
   }
-
 }
