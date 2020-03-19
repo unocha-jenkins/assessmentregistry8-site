617f32a13155fce27ac3fb78e7ed027534ea945a
Peter Droogmans (attiks) <peter@attiks.com>
2020-03-19 11:10:33 +0100

    Expose lat, lon on field

---
+++ html/modules/custom/ocha_integrations/ocha_locations/src/GetLocationLatLon.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\ocha_locations\GetLocationLatLon.
+ */
+
+namespace Drupal\ocha_locations;
+
+use Drupal\Core\TypedData\TypedData;
+
+/**
+ * A computed property for an average dice roll.
+ */
+class GetLocationLatLon extends TypedData {
+
+  /**
+   * Cached processed value.
+   *
+   * @var string|null
+   */
+  protected $processed = NULL;
+
+  /**
+   * Implements \Drupal\Core\TypedData\TypedDataInterface::getValue().
+   */
+  public function getValue($langcode = NULL) {
+    if ($this->processed !== NULL) {
+      return $this->processed;
+    }
+
+    $this->processed = '';
+
+    $item = $this->getParent();
+    $location = ocha_locations_get_item($item->value);
+    if ($location) {
+      $this->processed = $location->lon . ',' . $location->lat;
+      //$this->processed = 'POINT (' . $location->lon . ' ' . $location->lat . ')';
+    }
+
+    return $this->processed;
+  }
+
+  /**
+   * Implements \Drupal\Core\TypedData\TypedDataInterface::setValue().
+   */
+  public function setValue($value, $notify = TRUE) {
+    $this->processed = $value;
+
+    // Notify the parent of any changes.
+    if ($notify && isset($this->parent)) {
+      $this->parent->onChange($this->name);
+    }
+  }
+}
