d98990249f935620a58af7f671dca5e061172dde
Peter Droogmans (attiks) <peter@attiks.com>
2020-03-23 17:39:20 +0100

    Assessment component

---
+++ html/modules/custom/ocha_assessments/component/rollup.config.js
@@ -0,0 +1,13 @@
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+	input: ['src/ocha-assessments-table.js'],
+	output: {
+		file: 'build/ocha-assessments-table.js',
+    format: 'es',
+		sourcemap: true
+	},
+	plugins: [
+    resolve()
+  ]
+};
