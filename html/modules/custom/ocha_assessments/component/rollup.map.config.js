780a966a1931fbd21454e3e19e053f26b7b98a49
Peter Droogmans (attiks) <peter@attiks.com>
2020-03-26 19:42:23 +0100

    CS

--- html/modules/custom/ocha_assessments/component/rollup.map.config.js
+++ html/modules/custom/ocha_assessments/component/rollup.map.config.js
@@ -1,13 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-	input: ['src/ocha-assessments-map.js'],
-	output: {
-		file: 'build/ocha-assessments-map.js',
+  input: ['src/ocha-assessments-map.js'],
+  output: {
+    file: 'build/ocha-assessments-map.js',
     format: 'es',
-		sourcemap: true
-	},
-	plugins: [
-    resolve()
+    sourcemap: true
+  },
+  plugins: [
+  resolve()
   ]
 };
--- html/modules/custom/ocha_assessments/component/rollup.table.config.js
+++ html/modules/custom/ocha_assessments/component/rollup.table.config.js
@@ -1,13 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-	input: ['src/ocha-assessments-table.js'],
-	output: {
-		file: 'build/ocha-assessments-table.js',
+  input: ['src/ocha-assessments-table.js'],
+  output: {
+    file: 'build/ocha-assessments-table.js',
     format: 'es',
-		sourcemap: true
-	},
-	plugins: [
+    sourcemap: true
+  },
+  plugins: [
     resolve()
   ]
 };
