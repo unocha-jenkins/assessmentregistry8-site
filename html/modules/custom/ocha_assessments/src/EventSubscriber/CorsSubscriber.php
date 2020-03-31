7c1d00faccde35940e6865e1e1aa99eba3dcb88e
Peter Droogmans (attiks) <peter@attiks.com>
2020-03-31 13:43:56 +0200

    Allow credentials

--- html/modules/custom/ocha_assessments/src/EventSubscriber/CorsSubscriber.php
+++ html/modules/custom/ocha_assessments/src/EventSubscriber/CorsSubscriber.php
@@ -49,6 +49,7 @@ class CorsSubscriber implements EventSubscriberInterface {
 
     $response->headers->set('Access-Control-Allow-Origin', $host);
     $response->headers->set('Access-Control-Allow-Headers', 'Authorization, Accept, Accept-Language, Content-Language, Content-Type');
+    $response->headers->set('Access-Control-Allow-Credentials', 'true');
     $response->headers->set('Access-Control-Expose-Headers', '');
     $response->headers->set('Access-Control-Allow-Methods', 'GET');
     $response->headers->set('Access-Control-Max-Age', '120');
