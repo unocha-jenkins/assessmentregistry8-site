cd7b9418233b568a9759716aae831bc11e1296c0
Peter Droogmans (attiks) <peter@attiks.com>
2020-07-07 14:43:20 +0200

    AR-205: Update web component

--- html/modules/custom/ocha_map/component/ocha-assessments-map.js
+++ html/modules/custom/ocha_map/component/ocha-assessments-map.js
@@ -3661,6 +3661,7 @@ class OchaAssessmentsBase extends LitElement {
       local_groups: 'Local group',
       clusters_sectors: 'Cluster/sector',
       countries: 'Country',
+      disasters_emergencies: 'Disasters/Emergencies',
       disasters: 'Disaster',
       locations: 'Location',
       organizations: 'Organization',
@@ -19254,8 +19255,6 @@ class OchaAssessmentsMap extends OchaAssessmentsBase {
           @apply (--leaflet-map-component)
         }
       </style>
-
-      <p>Source (debug): ${this.src}</p>
 
       ${this.renderErrorMessage()}
 
